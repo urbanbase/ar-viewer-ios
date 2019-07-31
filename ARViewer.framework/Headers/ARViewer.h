51592ac651b9da510939c1ebe65d485635bf69bf
urbanbase <38779330+urbanbase@users.noreply.github.com>
2019-07-31 10:07:43 +0900

    add framework file

---
+++ ARViewer.framework/Headers/ARViewer.h
@@ -0,0 +1,19 @@
+//
+//  ARViewer.h
+//  ARViewer
+//
+//  Created by 이우석 on 18/12/2018.
+//  Copyright © 2018 이우석. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARViewer.
+FOUNDATION_EXPORT double ARViewerVersionNumber;
+
+//! Project version string for ARViewer.
+FOUNDATION_EXPORT const unsigned char ARViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARViewer/PublicHeader.h>
+
+
